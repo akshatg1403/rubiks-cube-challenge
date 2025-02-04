eeba149ec1b8c9ddddea29c2f8dfb5470979eb05
akshatg1403 <153669331+akshatg1403@users.noreply.github.com>
2025-02-05 03:40:53 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        @import url('https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap');
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
